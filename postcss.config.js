95487bb3c40586e5f14b11c1647e691f78793ea9
Philight <Philight@users.noreply.github.com>
2024-12-05 00:49:50 +0100

    Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,16 @@
 export default {
   plugins: {
+    // 'postcss-import': {
+      /*
+      root: path.resolve(__dirname, 'src'),
+//      path: ['app/_assets', 'app/_css'],
+      skipDuplicates: true,
+      resolve: (id, basedir, importOptions) => {
+        const [aliasName, filename] = id.split('/');
+        return aliasMapping[aliasName](filename);
+      }
+*/
+    // },    
     'postcss-nesting': {},
     'autoprefixer': {},
     'cssnano': {
