7a35c549ddeaa7820a084f2cf0e776623839587e
Philight <Philight@users.noreply.github.com>
2024-12-05 16:58:05 +0100

    TW

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,10 +75,10 @@ export default {
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: ':root', // The element that receives theme color CSS variables
   },
-  content: ['./src/**/*.{html,md,njk,ejs,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{html,md,njk,ejs,js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
