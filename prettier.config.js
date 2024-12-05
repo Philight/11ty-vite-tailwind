8ae823e7a9cd2f7890240b4ec2f2bfad5c332dd4
Philight <Philight@users.noreply.github.com>
2024-12-05 02:15:15 +0100

    lint

--- prettier.config.js
+++ prettier.config.js
@@ -20,8 +20,7 @@ export default {
   quoteProps: 'as-needed',
   plugins: [
     'prettier-plugin-tailwindcss',
-    // 'prettier-plugin-twig-nunjucks-melody',
-    // 'prettier-plugin-jinja-template',
+    'prettier-plugin-jinja-template',
     // '@trivago/prettier-plugin-sort-imports'
   ],
   // importOrderSeparation: true,
@@ -44,8 +43,7 @@ export default {
     {
       files: ['*.njk'],
       options: {
-        // parser: 'jinja-template',
-        // parser: 'twig-nunjucks-melody',
+        parser: 'jinja-template',
       },
     },
   ],
